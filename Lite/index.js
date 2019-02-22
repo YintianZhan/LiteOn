d66be02f9a0f9b4646c69cb362cde17973e62895
Yintian <yintianzhan2021@u.northwestern.edu>
2019-02-21 21:58:21 -0600

    simple header

--- Lite/index.js
+++ Lite/index.js
@@ -6,12 +6,12 @@ import React from 'react'
 // import ReactNative from 'react-native'
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+import App from './App';
 import {name as appName} from './app.json';
 import Header from './src/components/header'
 
-const App = () =>  (
-  <Header />
-);
+// const App = () =>  (
+//   <Header />
+// );
 
 AppRegistry.registerComponent(appName, () => App);
