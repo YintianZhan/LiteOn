08974bde813dc9903f4afdfd436863b229211fad
Yintian <yintianzhan2021@u.northwestern.edu>
2019-02-21 21:32:35 -0600

    simple header

---
+++ Lite/src/components/header.js
@@ -0,0 +1,10 @@
+//import libraries
+import React from 'react'
+import { Text } from 'react-native'
+//make a component
+const Header = () =>{
+  return <Text>Discover!</Text>
+};
+
+//make the compoent available to other parts of the app
+export default Header;
