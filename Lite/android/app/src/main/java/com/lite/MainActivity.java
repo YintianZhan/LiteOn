ea232015d80ae1bd06775f40b926b5060e7562d3
unknown <yintianzhan2021@u.northwestern.edu>
2019-04-01 06:23:54 +0100

    running android. just exists, does not swipe

--- Lite/android/app/src/main/java/com/lite/MainActivity.java
+++ Lite/android/app/src/main/java/com/lite/MainActivity.java
@@ -1,15 +1,20 @@
 package com.lite;
 
-import com.facebook.react.ReactActivity;
+//import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
 
-public class MainActivity extends ReactActivity {
+//public class MainActivity extends ReactActivity {
+//
+//    /**
+//     * Returns the name of the main component registered from JavaScript.
+//     * This is used to schedule rendering of the component.
+//     */
+//    @Override
+//    protected String getMainComponentName() {
+//        return "Lite";
+//    }
+//}
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "Lite";
-    }
+public class MainActivity extends NavigationActivity {
+
 }
