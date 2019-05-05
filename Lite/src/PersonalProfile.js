e53738e8e06a42ee932b981dfda2b8e226378e58
Tony Zhang <Johnson@dhcp-10-105-200-62.wireless.northwestern.private>
2019-05-04 21:26:01 -0500

    profile

--- Lite/src/PersonalProfile.js
+++ Lite/src/PersonalProfile.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component } from 'react';
 import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
 import { View } from 'react-native'
@@ -11,7 +12,7 @@ export default class PersonalProfile extends Component {
           <Left>
             <Button onPress={() => Navigation.dismissModal(this.props.componentId)}
                     transparent>
-              <Icon name='arrow-back' />
+              <Icon name= "arrow-back" />
             </Button>
           </Left>
           <Body>
@@ -23,3 +24,125 @@ export default class PersonalProfile extends Component {
     )
   }
 }
+*/
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+
+export default class ProfileView extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}>
+            <Button onPress={() => Navigation.dismissModal(this.props.componentId)}
+                    transparent>
+              <Icon name= "arrow-back" />
+            </Button>
+            <View style={styles.headerContent}>
+                <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar2.png'}}/>
+                <Text style={styles.name}>
+                  Tony Zhang
+                </Text>
+            </View>
+          </View>
+
+          <View style={styles.profileDetail}>
+            <View style={styles.detailContent}>
+              <Text style={styles.title}>Watched</Text>
+              <Text style={styles.count}>200</Text>
+            </View>
+            <View style={styles.detailContent}>
+              <Text style={styles.title}>Rated</Text>
+              <Text style={styles.count}>200</Text>
+            </View>
+          </View>
+
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 1</Text>  
+              </TouchableOpacity> 
+              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+            </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  header:{
+    backgroundColor: "#586589",
+  },
+  headerContent:{
+    padding:30,
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  profileDetail:{
+    alignSelf: 'center',
+    marginTop:200,
+    alignItems: 'center',
+    flexDirection: 'row',
+    position:'absolute',
+    backgroundColor: "#ffffff"
+  },
+  detailContent:{
+    margin:10,
+    alignItems: 'center'
+  },
+  title:{
+    fontSize:20,
+    color: "#586589"
+  },
+  count:{
+    fontSize:18,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+    marginTop:40
+  },
+  textInfo:{
+    fontSize:18,
+    marginTop:20,
+    color: "#696969",
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00CED1",
+  },
+  description:{
+    fontSize:20,
+    color: "#00CED1",
+    marginTop:10,
+    textAlign: 'center'
+  },
+});
